a07ffa8e1926570f8ec3bd52ae3d2d2f47f9a3a3
YashLagare <yashlagare77@gmail.com>
2025-06-24 19:12:25 +0530

    change in message route

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,17 +1,36 @@
+// import express from 'express';
+// import { getMessages, getUserForSidebar, sendMessage } from "../controllers/message.controller.js";
+// import { protectRoute } from '../middleware/auth.middleware.js';
+// const router = express.Router();
+
+// router.get("/users", protectRoute, getUserForSidebar);
+// router.get("/:id",protectRoute,getMessages);
+
+// router.post("/send/:id",protectRoute,sendMessage);
+
+
+
+
+
+
+
+// export default router;  // export the router so it can be used in other files
+
 import express from 'express';
 import { getMessages, getUserForSidebar, sendMessage } from "../controllers/message.controller.js";
 import { protectRoute } from '../middleware/auth.middleware.js';
+
 const router = express.Router();
 
 router.get("/users", protectRoute, getUserForSidebar);
-router.get("/:id",protectRoute,getMessages);
 
-router.post("/send/:id",protectRoute,sendMessage);
+// Add validation for the ID parameter
+router.get("/:id([0-9a-fA-F]{24})", protectRoute, getMessages); // For MongoDB ObjectId
+// OR for any non-empty string:
+// router.get("/:id([^/]+)", protectRoute, getMessages);
 
+router.post("/send/:id([0-9a-fA-F]{24})", protectRoute, sendMessage); // For MongoDB ObjectId
+// OR for any non-empty string:
+// router.post("/send/:id([^/]+)", protectRoute, sendMessage);
 
-
-
-
-
-
-export default router;  // export the router so it can be used in other files
+export default router;

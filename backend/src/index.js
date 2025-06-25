516c1402aa29afa13143a0ed0a6d43a38577382e
YashLagare <yashlagare77@gmail.com>
2025-06-25 14:13:57 +0530

    Final changes before deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -42,6 +42,11 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+
+import listEndpoints from 'express-list-endpoints';
+console.log(listEndpoints(app));
+
+
 // Start server
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
